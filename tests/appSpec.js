83a9b4fa9814eef230b5fa6ccb632f4f40320c2b
20064946 <ga.smith97@hotmail.co.uk>
2016-12-12 15:44:54 +0000

    First test passed

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -1,3 +1,10 @@
 /**
- * Created by davem on 27/11/2016.
+ * Created by Graeme Smith on 12/12/2016.
  */
+
+describe('LearnJavascript', function(){
+    it('can show question view', function(){
+        learnjavascript.showView('#quesion-1');
+        expect($('.view-container .question-view').length).toEqual(1);
+    });
+});
