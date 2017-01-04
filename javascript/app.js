5602beae8e8c1044574cacf623cfa6fdd4cd511a
Graeme <ga.smith97@hotmail.co.uk>
2017-01-04 12:42:29 +0000

    Tutorial part 4 complete!

--- javascript/app.js
+++ javascript/app.js
@@ -4,17 +4,89 @@
 
 'use strict';
 var learnjavascript={};
-learnjavascript.questionView = function(questionNumber){
-    var title= 'Question #'+questionNumber +' Arriving soon!!!';
-    return $('<div class="question-view" >').text(title);
+learnjavascript.questions=[
+    {
+        description:"What is truth?",
+        code:"function problem() {return __;}"
+    },
+    {
+        description:"Easy Maths?",
+        code:"function problem() {return 42 === 7* __;}"
+    }
+];
+learnjavascript.triggerEvent= function(name, args) {
+    $('.view-container>*').trigger(name, args);
+};
+learnjavascript.template = function(name){
+    return $('.templates .' + name).clone();
+};
+learnjavascript.applyObject= function(obj,elem){
+    for (var key in obj){
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
+learnjavascript.flashElement = function(elem, content){
+    elem.fadeOut('fast', function(){
+        elem.html(content);
+        elem.fadeIn();
+    });
+};
+learnjavascript.buildCorrectFlash = function(questionNum){
+    var correctFlash = learnjavascript.template('correct-flash');
+    var link =correctFlash.find('a');
+    if(questionNum < learnjavascript.questions.length){
+        link.attr('href','#question-'+(questionNum + 1));
+    }else{
+        link.attr('href', '');
+        link.text("You're Finished!")
+    }
+    return correctFlash;
+};
+learnjavascript.questionView = function(data){
+    var questionNumber =parseInt(data, 11);
+    var view= learnjavascript.template('question-view');
+    var questionData =learnjavascript.questions[questionNumber-1];
+    var resultFlash =view.find('.result');
+    function checkAnswer(){
+        var answer= view.find('.answer').val();
+        var test =questionData.code.replace('__', answer)+';problem();';
+        return eval(test);
+    }
+    function checkAnswerClick(){
+        if(checkAnswer()){
+            var flashContent =learnjavascript.buildCorrectFlash(questionNumber)
+            learnjavascript.flashElement( resultFlash,flashContent);
+        }else {
+            learnjavascript.flashElement(resultFlash,'InCorrect!');
+        }
+        return false;
+    }
+    if (questionNumber < learnjavascript.questions.length) {
+        var buttonItem = learnjavascript.template('skip-btn');
+        buttonItem.find('a').attr('href', '#question-' + (questionNumber + 1));
+        $('.nav-list').append(buttonItem);
+        view.bind('removingView', function() {
+            buttonItem.remove();
+        });
+    }
+    view.find('.check-btn').click(checkAnswerClick);
+    view.find('.title').text('Question #'+ questionNumber);
+    learnjavascript.applyObject(questionData,view);
+    return view;
+};
+learnjavascript.landingView= function(){
+    return learnjavascript.template('landing-view');
 };
 learnjavascript.showView = function(hash) {
     var routes = {
-        '#question': learnjavascript.questionView
+        '#question': learnjavascript.questionView,
+        '#': learnjavascript.landingView,
+        '':learnjavascript.landingView
     };
     var hashParts = hash.split('-');
     var viewFn = routes[hashParts[0]];
     if (viewFn) {
+        learnjavascript.triggerEvent('removingView', []);
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 };
@@ -24,4 +96,3 @@ learnjavascript.appOnReady = function() {
     };
     learnjavascript.showView(window.location.hash);
 };
-
--- tests/appSpec.js
+++ tests/appSpec.js
@@ -16,21 +16,80 @@ describe('Learn JavaScript', function(){
         learnjavascript.showView('#question-42');
         expect(learnjavascript.questionView).toHaveBeenCalledWith('42');
     });
-    describe('problem view', function(){
-        it('has a title that includes the question number',function(){
-            var view =learnjavascript.questionView('1');
-            expect(view.text()).toEqual('Question #1 Arriving soon!!!');
-        }) ;
-        it('invokes the router when loading', function(){
-            spyOn(learnjavascript,'showView');
-            learnjavascript.appOnReady();
-            expect(learnjavascript.showView).toHaveBeenCalledWith(window.location.hash);
-        });
-        it('subscribes to the hash change event', function(){
-            learnjavascript.appOnReady();
-            spyOn(learnjavascript,'showView');
-            $(window).on('hashchange', function(){}).trigger('hashchange');
-            expect(learnjavascript.showView).toHaveBeenCalledWith(window.location.hash);
-        });
+    it('invokes the router when loading', function(){
+        spyOn(learnjavascript,'showView');
+        learnjavascript.appOnReady();
+        expect(learnjavascript.showView).toHaveBeenCalledWith(window.location.hash);
     });
-});
+    it('subscribes to the hash change event', function(){
+        learnjavascript.appOnReady();
+        spyOn(learnjavascript,'showView');
+        $(window).on('hashchange', function(){}).trigger('hashchange');
+        expect(learnjavascript.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+    it('can flash an element while setting the text', function() {
+        var elem = $('<p>');
+        spyOn(elem, 'fadeOut').and.callThrough();
+        spyOn(elem, 'fadeIn');
+        learnjavascript.flashElement(elem, "new text");
+        expect(elem.text()).toEqual("new text");
+        expect(elem.fadeOut).toHaveBeenCalled();
+        expect(elem.fadeIn).toHaveBeenCalled();
+    });
+    it('can redirect to the main view after the last problem is answered', function() {
+        var flash = learnjavascript.buildCorrectFlash(2);
+        expect(flash.find('a').attr('href')).toEqual("");
+        expect(flash.find('a').text()).toEqual("You're Finished!");
+    });
+    it('can trigger events on the view', function() {
+        var callback = jasmine.createSpy('callback');
+        var div = $('<div>').bind('fooEvent', callback);
+        $('.view-container').append(div);
+        learnjavascript.triggerEvent('fooEvent', ['bar']);
+        expect(callback).toHaveBeenCalled();
+        expect(callback.calls.argsFor(0)[1]).toEqual('bar');
+    });
+    describe('question view', function(){
+        var view;
+        beforeEach(function() {
+            view = learnjavascript.questionView('1');
+        });
+        it('has a title that includes the question number',function(){
+//var view =learnjavascript.questionView('1');
+            expect(view.find('.title').text()).toEqual('Question #1');
+        }) ;
+        it('shows the description', function() {
+            expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+        });
+        it('shows the problem code', function() {
+            expect(view.find('[data-name="code"]').text()).toEqual('function problem() {return
+            __;}');
+    });
+    describe('answer section', function() {
+        var resultFlash;
+        beforeEach(function() {
+            spyOn(learnjavascript, 'flashElement');
+            resultFlash = view.find('.result');
+        });
+        describe('when the answer is correct', function() {
+            beforeEach(function() {
+                view.find('.answer').val('true');
+                view.find('.check-btn').click();
+            });
+            it('flashes the result', function() {
+                var flashArgs =learnjavascript.flashElement.calls.argsFor(0);
+                expect(flashArgs[0]).toEqual(resultFlash);
+                expect(flashArgs[1].find('span').text()).toEqual('Correct!');
+            });
+            it('shows a link to the next problem', function() {
+                var link = learnjavascript.flashElement.calls.argsFor(0)[1].find('a');
+                expect(link.text()).toEqual('Next Question');
+                expect(link.attr('href')).toEqual('#question-2');
+            });
+        });
+        it('rejects an incorrect answer', function() {
+            view.find('.answer').val('false');
+            view.find('.check-btn').click();
+            expect(learnjavascript.flashElement).toHaveBeenCalledWith(resultFlash,
+                'InCorrect!');
+        });
